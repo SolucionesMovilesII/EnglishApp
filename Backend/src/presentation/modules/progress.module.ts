ddcbfb212a8cb6c909bd71cb7e2e0f7dc12b5db9
Camila Cabrera <papu_serrana@outlook.com>
2025-09-11 01:17:56 -0500

    #9 hu-022--Endpoint /progress (CRUD)

---
+++ Backend/src/presentation/modules/progress.module.ts
@@ -0,0 +1,47 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserProgress } from '../../domain/entities/user-progress.entity';
+import { User } from '../../domain/entities/user.entity';
+import { ProgressController } from '../controllers/progress/progress.controller';
+import { CreateProgressUseCase } from '../../application/use-cases/progress/create-progress.use-case';
+import { GetUserProgressUseCase } from '../../application/use-cases/progress/get-user-progress.use-case';
+import { UpdateProgressUseCase } from '../../application/use-cases/progress/update-progress.use-case';
+import { UserProgressRepository } from '../../infrastructure/repositories/user-progress.repository';
+import { UserRepository } from '../../infrastructure/repositories/user.repository';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([UserProgress, User])
+  ],
+  controllers: [
+    ProgressController,
+  ],
+  providers: [
+    // Use Cases
+    CreateProgressUseCase,
+    GetUserProgressUseCase,
+    UpdateProgressUseCase,
+    
+    // Repositories
+    UserProgressRepository,
+    UserRepository,
+    
+    // Interface bindings
+    {
+      provide: 'IUserProgressRepository',
+      useClass: UserProgressRepository,
+    },
+    {
+      provide: 'IUserRepository',
+      useClass: UserRepository,
+    },
+  ],
+  exports: [
+    CreateProgressUseCase,
+    GetUserProgressUseCase,
+    UpdateProgressUseCase,
+    'IUserProgressRepository',
+    'IUserRepository',
+  ],
+})
+export class ProgressModule {}
